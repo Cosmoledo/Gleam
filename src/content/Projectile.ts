635889718abad9f4829f6e1021ab911f458c272a
Cosmoledo <marco.fvstein@web.de>
2026-05-25 14:33:22 +0200

    fix Projectile.ts: clone vel, drop dead initialiser, drop per-frame allocation in update

--- src/content/Projectile.ts
+++ src/content/Projectile.ts
@@ -9,20 +9,23 @@ export default class Projectile {
 	protected lifetime = 0;
 	protected pos: Vec2;
 	protected rotation = 0;
-	protected vel: Vec2 = new Vec2();
+	protected vel: Vec2;
+	protected _rect: Rect;
 
 	public get alive(): boolean {
 		return this.lifetime < this.maxLifetime;
 	}
 
 	public get rect(): Rect {
-		return this.pos.toRectAddSize(this.image.width, this.image.height);
+		return this._rect;
 	}
 
 	constructor(pos: Vec2, image: HTMLCanvasElement, vel: Vec2 = new Vec2()) {
 		this.pos = pos.clone();
 		this.image = image;
-		this.vel = vel;
+		this.vel = vel.clone();
+
+		this._rect = pos.toRectAddSize(this.image.width, this.image.height);
 
 		this.rotation = Math.atan2(this.vel.y, this.vel.x);
 	}
@@ -41,10 +44,13 @@ export default class Projectile {
 	public update(dt: number): void {
 		this.lifetime += dt;
 
-		this.pos.add(this.vel.clone().mult(this.speed * dt));
+		this.pos.x += this.vel.x * this.speed * dt;
+		this.pos.y += this.vel.y * this.speed * dt;
+
+		this._rect.set(this.pos.x, this.pos.y);
 	}
 
 	public remove(): void {
-		this.lifetime = Infinity;
+		this.lifetime = this.maxLifetime * 2;
 	}
 }
