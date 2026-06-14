4ae58a69bb72be787a99dfce91b5defcc6dfab93
Cosmoledo <marco.fvstein@web.de>
2026-06-14 15:11:54 +0200

    Audio: declare global per method, regions, JSDoc, defineMethod

--- src/prototypes/Audio.ts
+++ src/prototypes/Audio.ts
@@ -1,24 +1,40 @@
-export {};
+import { defineMethod } from "@/utilities/Prototype";
 
+// #region clone
 declare global {
 	interface HTMLAudioElement {
-		defaultVolume?: number;
+		/** Deep-clone this audio element, preserving the current `volume`. */
 		clone(): HTMLAudioElement;
-		stop: () => void;
 	}
 }
 
-HTMLAudioElement.prototype.clone = function clone(): HTMLAudioElement {
-	const node = this.cloneNode(true) as HTMLAudioElement;
-	node.volume = this.volume;
-	return node;
-};
+defineMethod(
+	HTMLAudioElement.prototype,
+	"clone",
+	function (): HTMLAudioElement {
+		const node = this.cloneNode(true) as HTMLAudioElement;
+		node.volume = this.volume;
+		return node;
+	},
+);
+// #endregion
 
-HTMLAudioElement.prototype.stop = function stop(): void {
+// #region stop
+declare global {
+	interface HTMLAudioElement {
+		/** Volume restored by `stop()` after pausing. If unset, `stop()` leaves `volume` as-is. */
+		defaultVolume?: number;
+		/** Pause playback, reset `currentTime` to 0, and restore `volume` to `defaultVolume` if set. */
+		stop(): void;
+	}
+}
+
+defineMethod(HTMLAudioElement.prototype, "stop", function (): void {
 	this.pause();
 	this.currentTime = 0;
 
 	if (this.defaultVolume !== undefined) {
 		this.volume = this.defaultVolume;
 	}
-};
+});
+// #endregion
